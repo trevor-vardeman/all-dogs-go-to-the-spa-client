7d2fbebbcb921bfb7356ea5a1cb5715301e09f5e
Trevor Vardeman <trevortx@gmail.com>
2022-07-13 00:46:04 -0700

    added dogs and groomers components

--- src/components/Dogs.js
+++ src/components/Dogs.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react'
 import Navbar from './Navbar'
 
 function Dogs() {
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/dogs")
+      .then(r => r.json())
+      .then(data => setDogs(data))
+      .catch(err => alert(err.message))
+  },[])
+
   return (
     <div>
       <Navbar />
-      <h1>Dogs</h1>
+      <div>
+        <h2>Dogs</h2>
+        {dogs.map((dog) => (
+          <div key={dog.id}>
+            <h6><strong>{dog.name}</strong></h6>
+            <h6>Breed: {dog.breed}</h6>
+            <h6>Age: {dog.age}</h6>
+            <img style={{maxWidth: "400px", maxHeight: "400px"}} src={dog.photo_url} alt={`${dog.name} the ${dog.breed}`}/>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Groomers.js
+++ src/components/Groomers.js
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react'
 import Navbar from './Navbar'
 
 function Groomers() {
+  const [groomers, setGroomers] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/groomers")
+      .then(r => r.json())
+      .then(data => setGroomers(data))
+      .catch(err => alert(err.message))
+  },[])
+
   return (
     <div>
       <Navbar />
-      <h1>Groomers</h1>
+      <div>
+        <h2>Groomers</h2>
+        {groomers.map((groomer) => (
+          <div key={groomer.id}>
+            <h6><strong>{groomer.name}</strong></h6>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
